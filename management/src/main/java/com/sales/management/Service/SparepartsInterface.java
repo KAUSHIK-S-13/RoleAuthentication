b02c38d8124638ba14f9d8e6619d3a73e1955d52
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2021-09-22 14:50:15 +0530

    Add files via upload

---
+++ management/src/main/java/com/sales/management/Service/SparepartsInterface.java
@@ -0,0 +1,20 @@
+package com.sales.management.Service;
+
+import com.sales.management.BaseResponse.APIResponse;
+import com.sales.management.DTO.SparepartsDTO;
+import com.sales.management.Model.Spareparts;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public interface SparepartsInterface {
+    Spareparts addSpareparts(SparepartsDTO sparepartsDTO);
+
+    Optional<Spareparts> updateSpareparts(SparepartsDTO sparepartsDTO);
+
+    Optional<Spareparts> findSparepartsById(int id);
+
+    Optional<Spareparts> deleteSpareparts(SparepartsDTO sparepartsDTO);
+
+    APIResponse<Spareparts> sparepatswithpage(int offset, int pageSize, String sparepartsName);
+}
